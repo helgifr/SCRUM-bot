475d93959fabbe63b6869e868424cbace317afc2
Ytreg <helgifreyr95@gmail.com>
2018-02-12 21:26:41 +0000

    done

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ let date = 'Engin dagsetning';
 
 bot.on('message', (message) => {
     if (message.content === '!date') {
-        message.channel.sendMessage(date);
+        message.channel.send(date);
     } else if (message.content.substring(0,8)  === '!newDate') {
         date = message.content.substring(9);
         let newDate = `Nýja dagsetningin er: ${date}`;
-        message.channel.sendMessage(newDate);
+        message.channel.send(newDate);
     } else if (message.content === '!help') {
-		message.channel.sendMessage(`Skipanir eru !date og !newDate`);
+		message.channel.send(`Skipanir eru !date og !newDate`);
 	}
 });
 
